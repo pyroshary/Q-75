69acb75372b19ea4aca9f4bcb77b8f51e48a67e5
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-04 01:18:32 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+function useCompundoperators() {
+    let x = 5;
+    console.log("Initial value of X is", x);
+    x += 2;
+    console.log("After addition value of X is", x);
+    x -= 2;
+    console.log("After substraction value of X is", x);
+    x *= 2;
+    console.log("After multiply value of X is", x);
+    x /= 2;
+    console.log("After division value of X is", x);
+
+}
+useCompundoperators()
